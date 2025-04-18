9b460c192f0f166ef99721304457d7e24cb40583
Huang, Yuanpeng J <huangy26@rpi.edu>
2025-04-17 17:51:29 -0700

    Update run_doc1_noN.sh

--- CDK2AP-doc1/run_doc1_noN.sh
+++ CDK2AP-doc1/run_doc1_noN.sh
@@ -3,5 +3,5 @@
 #SBATCH --nodes=1
 source /gpfs/u/barn/PMAR/shared/etc/231_alphaFOLD
 
-sh ../scripts/enhancedSampling/run_afsample6000.sh doc1_noN.fasta
+sh ../scripts/run_afsample6000.sh doc1_noN.fasta
 
